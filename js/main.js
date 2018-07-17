f3d392ef68ff84cf28ceb75dccd0a29e8b3c17d6
ChenDeli <419287484@qq.com>
2018-07-17 17:49:59 +0800

    bootstrap

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ jQuery(document).ready(function () {
 // 百度地图
 // 百度地图API功能
 var map = new BMap.Map("qingxiongdizhi");    // 创建Map实例
-map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+map.centerAndZoom(new BMap.Point(113.26205, 23.131524), 18);  // 初始化地图,设置中心点坐标和地图级别
 //添加地图类型控件
 map.addControl(new BMap.MapTypeControl({
 	mapTypes: [
@@ -138,5 +138,11 @@ map.addControl(new BMap.MapTypeControl({
 		BMAP_HYBRID_MAP
 	]
 }));
-map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+
+var local = new BMap.LocalSearch(map, {
+	renderOptions: { map: map }
+});
+local.search("青雄地毯");
+map.addControl(new BMap.NavigationControl()); 
+map.setCurrentCity("广州");          // 设置地图显示的城市 此项是必须设置的
 map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
