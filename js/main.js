57cd1bf40ae4359fe6eb7135e30437ceb416121f
ChenDeli <419287484@qq.com>
2018-07-17 11:43:40 +0800

    导航菜单

--- js/main.js
+++ js/main.js
@@ -105,6 +105,28 @@ var wow = new WOW({
 // 	})
 
 // });
+
+// 导航菜单
+jQuery(document).ready(function () {
+	if ($('.cd-stretchy-nav').length > 0) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+
+		stretchyNavs.each(function () {
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+
+			stretchyNavTrigger.on('click', function (event) {
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
+
+		$(document).on('click', function (event) {
+			(!$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span')) && stretchyNavs.removeClass('nav-is-visible');
+		});
+	}
+});
+
 // 百度地图
 // 百度地图API功能
 var map = new BMap.Map("qingxiongdizhi");    // 创建Map实例
