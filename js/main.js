7fbf942bdb0c927bc688b4ff6fad4b90cb1d2c57
ChenDeli <419287484@qq.com>
2019-01-17 16:04:01 +0800

    结构和跳转整理

--- js/main.js
+++ js/main.js
@@ -1,120 +1,4 @@
-jQuery(document).ready(function($){
-	var mainHeader = $('.cd-auto-hide-header'),
-		secondaryNavigation = $('.cd-secondary-nav'),
-		//this applies only if secondary nav is below intro section
-		belowNavHeroContent = $('.sub-nav-hero'),
-		headerHeight = mainHeader.height();
-	
-	//set scrolling variables
-	var scrolling = false,
-		previousTop = 0,
-		currentTop = 0,
-		scrollDelta = 10,
-		scrollOffset = 150;
 
-	mainHeader.on('click', '.nav-trigger', function(event){
-		// open primary navigation on mobile
-		event.preventDefault();
-		mainHeader.toggleClass('nav-open');
-	});
-
-	$(window).on('scroll', function(){
-		if( !scrolling ) {
-			scrolling = true;
-			(!window.requestAnimationFrame)
-				? setTimeout(autoHideHeader, 250)
-				: requestAnimationFrame(autoHideHeader);
-		}
-	});
-
-	$(window).on('resize', function(){
-		headerHeight = mainHeader.height();
-	});
-
-	function autoHideHeader() {
-		var currentTop = $(window).scrollTop();
-
-		( belowNavHeroContent.length > 0 ) 
-			? checkStickyNavigation(currentTop) // secondary navigation below intro
-			: checkSimpleNavigation(currentTop);
-
-	   	previousTop = currentTop;
-		scrolling = false;
-	}
-
-	function checkSimpleNavigation(currentTop) {
-		//there's no secondary nav or secondary nav is below primary nav
-	    if (previousTop - currentTop > scrollDelta) {
-	    	//if scrolling up...
-	    	mainHeader.removeClass('is-hidden');
-	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-	    	//if scrolling down...
-	    	mainHeader.addClass('is-hidden');
-	    }
-	}
-
-	function checkStickyNavigation(currentTop) {
-		//secondary nav below intro section - sticky secondary nav
-		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
-		
-		if (previousTop >= currentTop ) {
-	    	//if scrolling up... 
-	    	if( currentTop < secondaryNavOffsetTop ) {
-	    		//secondary nav is not fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('fixed slide-up');
-	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
-	    	} else if( previousTop - currentTop > scrollDelta ) {
-	    		//secondary nav is fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-	    	
-	    } else {
-	    	//if scrolling down...	
-	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
-	 	  		//hide primary nav
-	    		mainHeader.addClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	} else if( currentTop > secondaryNavOffsetTop ) {
-	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-
-	    }
-	}
-});
-
-
-// 导航菜单
-jQuery(document).ready(function () {
-	// $(".cd-nav-trigger").draggable();
-	if ($('.cd-stretchy-nav').length > 0) {
-
-		var stretchyNavs = $('.cd-stretchy-nav');
-
-		stretchyNavs.each(function () {
-			var stretchyNav = $(this),
-				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
-
-			stretchyNavTrigger.on('click', function (event) {
-				event.preventDefault();
-				stretchyNav.toggleClass('nav-is-visible');
-			});
-		});
-
-		$(document).on('click', function (event) {
-			(!$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span')) && stretchyNavs.removeClass('nav-is-visible');
-		});
-	}
-});
-// $(function () {
-// 	$("#drag").draggable();
-// });
 // 组件高度
 $(".bodyComponents").height(window.innerHeight-70);
 $(".CompanyProfile-picture").children().width(window.innerWidth*0.6);
@@ -124,3 +8,40 @@ $(".CompanyProfile-picture").children().height(360);
 // 	width:window.innerWidth,
 // 	float:left
 // });
+
+// new WOW().init();
+var wow = new WOW({
+	boxClass: 'wow',
+	animateClass: 'animated',
+	offset: 0,
+	mobile: true,
+	live: true
+});
+wow.init();
+
+// $(function () {
+
+// 	var $gallery = $('.gallery').flickity({
+// 		cellSelector: '.gallery-cell'
+// 	})
+
+// });
+// 百度地图
+// 百度地图API功能
+var map = new BMap.Map("qingxiongdizhi");    // 创建Map实例
+map.centerAndZoom(new BMap.Point(113.26205, 23.131524), 18);  // 初始化地图,设置中心点坐标和地图级别
+//添加地图类型控件
+map.addControl(new BMap.MapTypeControl({
+	mapTypes: [
+		BMAP_NORMAL_MAP,
+		BMAP_HYBRID_MAP
+	]
+}));
+
+var local = new BMap.LocalSearch(map, {
+	renderOptions: { map: map }
+});
+local.search("青雄地毯");
+map.addControl(new BMap.NavigationControl());
+map.setCurrentCity("广州");          // 设置地图显示的城市 此项是必须设置的
+map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
